a33ef3ea8b3f22efcb1eb2940a37005d5633466d
mayonezny <borischubuhchiev@mail.ru>
2025-02-06 16:31:26 +0300

    добавлено время поста нормальное

--- src/app/components/blog/blogCard.tsx
+++ src/app/components/blog/blogCard.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
-
+import { format } from 'date-fns';
+import { ru } from 'date-fns/locale';
 const Card = ({ href, pic, title, author, date }) => {
 
   return (
@@ -8,7 +9,11 @@ const Card = ({ href, pic, title, author, date }) => {
       <Link href={href}>
         <div className="absolute rounded-lg flex flex-col justify-between p-5 inset-0 bg-gradient-to-b from-white/100 via-white/30 to-black/100">
           <h1 className="font-bold text-[22px] font-[family-name:var(--font-roboto-c)]">{title}</h1>
-          <h1 className="font-bold text-[22px] font-[family-name:var(--font-roboto-c)] text-white">{author && `Автор: ${author}`}</h1>
+          <div className='flex justify-between'>
+            <h1 className="font-bold text-[22px] font-[family-name:var(--font-roboto-c)] text-white">{author && `Автор: ${author}`}</h1>
+            <h1 className="font-bold text-[20px] font-[family-name:var(--font-roboto-c)] text-white">{date && `${format(date, 'dd MMMM, HH:mm', { locale: ru })}`}</h1>
+          </div>
+
         </div>
       </Link>
     </div>
--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 const Header = () => {
 
   return (
-    <nav className="sticky top-0 z-50 shadow-md bg-white flex justify-between items-center w-full text-xl 2xl:px-5 p-1 xl:px-3">
+    <nav className="sticky top-0 z-50 shadow-md bg-white flex justify-between items-center w-full text-xl 2xl:px-5 p-1 xl:px-2">
       <div className="flex gap-8 items-center">
         <NavButton href='/' className='px-0 py-0'>
           <Image
