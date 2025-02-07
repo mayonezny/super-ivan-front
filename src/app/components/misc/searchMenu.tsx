d6c64c48a8134888e71a4fddc4dff957cecbc243
mayonezny <borischubuhchiev@mail.ru>
2025-02-07 14:09:00 +0300

    удалили редкус нахрен

--- src/app/components/misc/searchMenu.tsx
+++ src/app/components/misc/searchMenu.tsx
@@ -3,8 +3,9 @@ import Search from '../../../../public/icons/search';
 const SearchMenu = () => {
 
   return (
-    <div className='flex justify-between items-center w-56 p-2 shadow-[0px_0px_3px_rgba(0,0,0,0.2)] rounded-full bg-white'>
+    <div className='flex gap-2 w-[65%] items-center p-2 shadow-[0px_0px_3px_rgba(0,0,0,0.2)] rounded-full bg-white'>
       <Search/>
+      <input className='focus:outline-none max-w-full w-full pr-1 text-lg'></input>
     </div>
   );
 };
