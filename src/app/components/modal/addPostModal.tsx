70639b96f54c219565b28cab0373095a1e32e7b6
mayonezny <borischubuhchiev@mail.ru>
2025-02-14 13:51:15 +0300

    ура, на клиенте модалка добавления поста в принципе готова

--- src/app/components/modal/addPostModal.tsx
+++ src/app/components/modal/addPostModal.tsx
@@ -1,11 +1,38 @@
+/* eslint-disable no-magic-numbers */
 'use client';
 
 import { useEffect, useState } from 'react';
 import Button from '../buttons/button';
+import ImageUploader from '../services/imageUploader';
+import * as yup from 'yup';
+import { useForm, Controller } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import postsStore from 'imp/store/PostsStore';
+import clsx from 'clsx';
+
+const addPostSchema = yup.object({
+  title: yup.string().min(6, 'Заголовок должен содержать не менее 6 символов!').max(30, 'Заголовок должен содержать не более 30 символов!').required('Пожалуйста, укажите заголовок'),
+  content: yup.string().min(20, 'Содержимое поста должно быть не менее 20 символов!').max(1000, 'Обалдеть какое здоровое содержимое!').required('Пожалуйста, укажите содержание поста'),
+  pic: yup.mixed().required('Эй! Картинка тоже обязательна!'),
+}).required();
 
 const AddPostModal = ({ isOpen, onClose }: { isOpen: boolean; onClose: () => void }) => {
+  const {
+    control,
+    register,
+    reset,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(addPostSchema), // передаем схему валидации
+  });
   const [visible, setVisible] = useState(false);
   const [showAnimation, setShowAnimation] = useState(false);
+
+  const handlePostUpload = (data) => {
+    // postsStore.addPost();
+    console.log(data);
+  };
 
   useEffect(() => {
     if (isOpen) {
@@ -20,38 +47,51 @@ const AddPostModal = ({ isOpen, onClose }: { isOpen: boolean; onClose: () => voi
   if (!visible) return null;
 
   return (
-    <div
-      className={`fixed inset-0 flex items-center justify-center z-50 transition-opacity duration-700  ${
-        showAnimation ? 'opacity-100' : 'opacity-0 pointer-events-none'
-      }`}
-    >
-      {/* Оверлей */}
+    <form onSubmit={handleSubmit(handlePostUpload)}>
       <div
-        className={`absolute inset-0 bg-black bg-opacity-50 transition-opacity duration-700 ${
-          showAnimation ? 'opacity-100' : 'opacity-0'
-        }`}
-        onClick={onClose}
-      ></div>
-
-      {/* Контент модалки */}
-      <div
-        className={`flex flex-col gap-5 bg-white p-6 rounded-xl shadow-lg z-10 w-[40%] h-[80%] transition-all duration-700 transform ${
-          showAnimation ? 'scale-100 opacity-100' : 'scale-95 opacity-0'
+        className={`fixed inset-0 flex items-center justify-center z-50 transition-opacity duration-700  ${
+          showAnimation ? 'opacity-100' : 'opacity-0 pointer-events-none'
         }`}
       >
-        <h2 className="text-3xl font-medium text-center">Новый пост:</h2>
-        <input className='bg-[#f0f0f0] rounded-lg w-full p-3 focus:outline-none font-[family-name:var(--font-roboto-c)] placeholder:px-[2px] placeholder:text-[#a0a0a0]' placeholder='Заголовок...'></input>
-        <textarea className='bg-[#f0f0f0] rounded-lg w-full h-full p-3 font-[family-name:var(--font-roboto-c)] resize-none text-clip focus:outline-none placeholder:px-[2px] placeholder:text-[#a0a0a0]' placeholder='Контент...'></textarea>
-        <input className='bg-[#f0f0f0] rounded-lg w-full p-3 focus:outline-none font-[family-name:var(--font-roboto-c)] placeholder:px-[2px] placeholder:text-[#a0a0a0]' placeholder='Картинка... (норм поле в разработке)'></input>
-        <Button className='bg-blue-700 text-white hover:bg-blue-800'>Опубликовать</Button>
-        {/* <button
+        {/* Оверлей */}
+        <div
+          className={`absolute inset-0 bg-black bg-opacity-50 transition-opacity duration-700 ${
+            showAnimation ? 'opacity-100' : 'opacity-0'
+          }`}
+          onClick={() => {
+            onClose(); reset();
+          }}
+        ></div>
+
+        {/* Контент модалки */}
+        <div
+          className={clsx(`flex flex-col bg-white p-6 rounded-xl shadow-lg z-10 w-[40%] h-[80%] transition-all duration-700 transform ${
+            showAnimation ? 'scale-100 opacity-100' : 'scale-95 opacity-0'
+          }`, (errors.title || errors.content || errors.pic) ? 'gap-2' : 'gap-5')}
+        >
+          <h2 className="text-3xl font-medium text-center">Новый пост:</h2>
+          <input {...register('title')} className={clsx('bg-[#f0f0f0] rounded-lg flex-[0.1] w-full p-2 xl:p-3 focus:outline-none font-[family-name:var(--font-roboto-c)] placeholder:px-[2px] placeholder:text-[#a0a0a0]')} placeholder='Заголовок...'></input>
+          {errors.title && <span className='flex text-red-500 text-sm flex-[0.1] items-center'>{errors.title.message}</span>}
+          <textarea {...register('content')} className={clsx('bg-[#f0f0f0] rounded-lg flex-1 w-full h-full p-2 xl:p-3 font-[family-name:var(--font-roboto-c)] resize-none text-clip focus:outline-none placeholder:px-[2px] placeholder:text-[#a0a0a0]')} placeholder='Контент...'></textarea>
+          {errors.content && <span className='flex text-red-500 text-sm flex-[0.1] items-center'>{errors.content.message}</span>}
+          <Controller
+            name='pic'
+            control={control}
+            render={({ field }) => (
+              <ImageUploader controllerField={field} className='flex-1'/>
+            )}/>
+          {errors.pic && <span className='flex text-red-500 text-sm flex-[0.1] items-center relative -top-2'>{errors.pic.message}</span>}
+          {/* <input className='bg-[#f0f0f0] rounded-lg w-full p-3 focus:outline-none font-[family-name:var(--font-roboto-c)] placeholder:px-[2px] placeholder:text-[#a0a0a0]' placeholder='Картинка... (норм поле в разработке)'></input> */}
+          <Button type='submit' className='bg-blue-700 text-white hover:bg-blue-800'>Опубликовать</Button>
+          {/* <button
           className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
           onClick={onClose}
         >
             Закрыть
         </button> */}
+        </div>
       </div>
-    </div>
+    </form>
   );
 };
 export default AddPostModal;
---
+++ src/app/components/services/imageUploader.tsx
@@ -0,0 +1,49 @@
+'use client';
+import { useState } from 'react';
+import { FilePond, registerPlugin } from 'react-filepond';
+import 'filepond/dist/filepond.min.css';
+import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css';
+import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
+import FilePondPluginFileValidateSize from 'filepond-plugin-file-validate-size';
+import { FilePondFile } from 'filepond';
+
+registerPlugin(FilePondPluginFileValidateType, FilePondPluginFileValidateSize);
+
+const ImageUploader = ({ controllerField, className }) => {
+
+  const handleFileSet = (files) => {
+    controllerField.onChange(files.length > 0 ? files[0] : null);
+    console.log('ivanva', files);
+  };
+
+  return (
+    <FilePond
+      className={className}
+      onupdatefiles={handleFileSet}
+      acceptedFileTypes={['image/jpeg', 'image/png', 'image/gif']}
+      fileValidateTypeLabelExpectedTypes="Попробуешь загрузить какой-нибудь другой файл - я тебя пиздану"
+      allowMultiple={false}
+      maxFileSize='3MB'
+      labelMaxFileSizeExceeded="Файл слишком большой"
+      labelMaxFileSize="Максимальный размер: 3MB"
+      name="file"
+      labelIdle='<span class="filepond-label">Кликай на меня чтобы загрузить фото поста (или просто дропни его сюда)</span>'
+      credits={false}
+      server={{
+        process: (fieldName, file, metadata, load, error, progress) => {
+          let progressValue = 0;
+          const interval = setInterval(() => {
+            progress(true, progressValue, 100); // Эмулируем прогресс
+            progressValue += 10;
+            if (progressValue > 100) {
+              clearInterval(interval);
+              load('done'); // Завершаем загрузку
+            }
+          }, 200);
+        },
+      }}
+    />
+  );
+};
+
+export default ImageUploader;
