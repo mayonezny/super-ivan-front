a5de29495f77cf35a0fd2c4ae8770c3cbbde241c
mayonezny <borischubuhchiev@mail.ru>
2025-03-11 12:47:46 +0300

    пофикшена плашка выдвижного текста дабы на всех устройствах выглядело добротно

--- src/app/components/profile/profilePreferencesNavbar.tsx
+++ src/app/components/profile/profilePreferencesNavbar.tsx
@@ -13,7 +13,7 @@ const ProfilePreferencesNavbar = ({ disabled, className }: ProfilePreferencesNav
   const [isDeleteHovered, setDeleteHovered] = useState(false);
   const [isLogoutHovered, setLogoutHovered] = useState(false);
   return (
-    <div>
+    <div className='relative'>
       <div className={
         clsx('flex flex-col items-center gap-4 bg-white p-2 py-3 shadow-[0px_0px_3px_rgba(0,0,0,0.2)] rounded-xl font-[family-name:var(--font-inter)]',
           disabled ? 'opacity-0 pointer-events-none' : '',
@@ -32,9 +32,9 @@ const ProfilePreferencesNavbar = ({ disabled, className }: ProfilePreferencesNav
         </div>
 
       </div>
-      <div className={clsx('-z-[1] absolute top-[180px] right-[512px] bg-white pl-8 p-2 shadow-[0px_0px_3px_rgba(0,0,0,0.2)] rounded-xl', isEditHovered ? 'animate-button-text-enter' : 'animate-button-text-leave')}>Редактировать профиль</div>
-      <div className={clsx('-z-[1] absolute top-[225px] right-[567px] bg-white pl-8 p-2 shadow-[0px_0px_3px_rgba(0,0,0,0.2)] rounded-xl', isDeleteHovered ? 'animate-button-text-enter' : 'animate-button-text-leave')}>Удалить профиль</div>
-      <div className={clsx('-z-[1] absolute top-[270px] right-[655px] bg-white pl-8 p-2 shadow-[0px_0px_3px_rgba(0,0,0,0.2)] rounded-xl', isLogoutHovered ? 'animate-button-text-enter' : 'animate-button-text-leave')}>Выйти</div>
+      <div className={clsx('-z-[1] absolute top-[12%] right-[-200%] text-nowrap bg-white pl-8 p-2 shadow-[0px_0px_3px_rgba(0,0,0,0.2)] rounded-xl', isEditHovered ? 'animate-button-text-enter' : 'animate-button-text-leave')}>Редактировать профиль</div>
+      <div className={clsx('-z-[1] absolute top-[42%] right-[-75%] text-nowrap bg-white pl-8 p-2 shadow-[0px_0px_3px_rgba(0,0,0,0.2)] rounded-xl', isDeleteHovered ? 'animate-button-text-enter' : 'animate-button-text-leave')}>Удалить профиль</div>
+      <div className={clsx('-z-[1] absolute top-[72%] right-[135%] text-nowrap bg-white pl-8 p-2 shadow-[0px_0px_3px_rgba(0,0,0,0.2)] rounded-xl', isLogoutHovered ? 'animate-button-text-enter' : 'animate-button-text-leave')}>Выйти</div>
     </div>
   );
 };
