9683139130f6fb313d6b1b67a906d62656fef248
mayonezny <borischubuhchiev@mail.ru>
2025-03-18 20:03:58 +0300

    подредачена модалка редактирования профиля (ласт коммит я ошибся - какое редактирование постов емае)

--- src/app/components/modal/accountEditModal.tsx
+++ src/app/components/modal/accountEditModal.tsx
@@ -48,22 +48,29 @@ const AccountEditModal = ({ isOpen, onClose }: { isOpen: boolean; onClose: () =>
         }`)}
       >
         <h1 className='text-2xl text-center mb-6'>Редактировать профиль:</h1>
-        <div className='flex flex-col gap-2'>
-          <h1 className='text-xl'>Меня зовут:</h1>
-          <input className={clsx('bg-[#f0f0f0] rounded-lg w-full p-2 xl:p-3 focus:outline-none font-[family-name:var(--font-roboto-c)] placeholder:px-[2px] placeholder:text-[#a0a0a0] text-lg')} defaultValue='Ваня' placeholder='Мне нужно знать твое имя чтобы навести порчу...'></input>
+        <div className='flex-1 grid grid-cols-2 gap-x-6 gap-y-3 content-start'>
+          <div className='flex flex-col gap-2'>
+            <h1 className='text-xl'>Меня зовут:</h1>
+            <input className={clsx('bg-[#f0f0f0] rounded-lg w-full p-2 xl:p-3 focus:outline-none font-[family-name:var(--font-roboto-c)] placeholder:px-[2px] placeholder:text-[#a0a0a0] text-lg')} defaultValue='Ваня' placeholder='Мне нужно знать твое имя чтобы навести порчу...'></input>
+          </div>
+          <div className='flex flex-col gap-2'>
+            <h1 className='text-xl'>Моя почта:</h1>
+            <input className={clsx('bg-[#f0f0f0] rounded-lg w-full p-2 xl:p-3 focus:outline-none font-[family-name:var(--font-roboto-c)] placeholder:px-[2px] placeholder:text-[#a0a0a0] text-lg')} defaultValue='ivanzapara04@mail.ru' placeholder='Это для ФСБ...'></input>
+          </div>
+          <div className='flex flex-col gap-2'>
+            <h1 className='text-xl'>Моя группа:</h1>
+            <input className={clsx('bg-[#f0f0f0] rounded-lg w-full p-2 xl:p-3 focus:outline-none font-[family-name:var(--font-roboto-c)] placeholder:px-[2px] placeholder:text-[#a0a0a0] text-lg')} defaultValue='ВПР32' placeholder='А группу чтобы сообщить в деканат...'></input>
+          </div>
+          <div className='flex flex-col gap-2'>
+            <h1 className='text-xl'>У меня днюха:</h1>
+            <input className={clsx('bg-[#f0f0f0] rounded-lg w-full p-2 xl:p-3 focus:outline-none font-[family-name:var(--font-roboto-c)] placeholder:px-[2px] placeholder:text-[#a0a0a0] text-lg')} defaultValue='11.09.2001' placeholder='А тут я просто хочу узнать о тебе побольше... ♥'></input>
+          </div>
+          <div className='flex flex-col h-full gap-2 col-span-2 row-span-2 2xl:min-h-[350px] xl:min-h-[200px] '>
+            <h1 className='text-xl'>Что я хочу сказать этому миру:</h1>
+            <textarea className={clsx('bg-[#f0f0f0] rounded-lg flex-1 w-full h-full p-2 xl:p-3 font-[family-name:var(--font-roboto-c)] resize-none text-clip focus:outline-none placeholder:px-[2px] placeholder:text-[#a0a0a0] text-lg')} defaultValue='Дарова, я Иванко' placeholder='Давай, расскажи о себе что-то новое...'></textarea>
+          </div>
         </div>
-        <div className='flex flex-col gap-2'>
-          <h1 className='text-xl'>Моя группа:</h1>
-          <input className={clsx('bg-[#f0f0f0] rounded-lg w-full p-2 xl:p-3 focus:outline-none font-[family-name:var(--font-roboto-c)] placeholder:px-[2px] placeholder:text-[#a0a0a0] text-lg')} defaultValue='ВПР32' placeholder='А группу чтобы сообщить в деканат...'></input>
-        </div>
-        <div className='flex flex-col gap-2'>
-          <h1 className='text-xl'>У меня днюха:</h1>
-          <input className={clsx('bg-[#f0f0f0] rounded-lg w-full p-2 xl:p-3 focus:outline-none font-[family-name:var(--font-roboto-c)] placeholder:px-[2px] placeholder:text-[#a0a0a0] text-lg')} defaultValue='11.09.2001' placeholder='А тут я просто хочу узнать о тебе побольше... ♥'></input>
-        </div>
-        <div className='flex flex-col gap-2 flex-1'>
-          <h1 className='text-xl'>Что я хочу сказать этому миру:</h1>
-          <textarea className={clsx('bg-[#f0f0f0] rounded-lg flex-1 w-full h-full p-2 xl:p-3 font-[family-name:var(--font-roboto-c)] resize-none text-clip focus:outline-none placeholder:px-[2px] placeholder:text-[#a0a0a0] text-lg')} defaultValue='Дарова, я Иванко' placeholder='Давай, расскажи о себе что-то новое...'></textarea>
-        </div>
+
         <div className='flex gap-2'>
           <button className='p-2 px-4 bg-blue-600 hover:bg-blue-700 duration-300 rounded-md text-white'>Подтвердить</button>
           <button onClick={() => {onClose();}} className='p-2 px-4 shadow-[0px_0px_3px_rgba(0,0,0,0.4)] rounded-md'>Отмена</button>
